ee17b54001ae9a314f386eb7e62734544f8e344f
root <root@example.com>
2015-01-28 15:52:22 +0800

    worker last commit

--- WorkerParallelModel.py
+++ WorkerParallelModel.py
@@ -6,6 +6,7 @@ import Queue
 import time
 import socket
 from py_utilbox import TcpServer
+import sys
 class New(TcpServer.TcpServer):
 	def __init__(self,workerfunc,mode="finish",process=1,overseer={"status":False}):
 		self.MANAGER = multiprocessing.Manager()
@@ -16,12 +17,13 @@ class New(TcpServer.TcpServer):
 		self.PROCESS = process
 		self.WORKERMODE = mode
 		self.WORKERS=[]
+		self.status=[]
 	#	self.WORLERS=self.MANAGER.list()
 		self.OVERSEER = overseer
 		TcpServer.TcpServer.__init__(self,port=self.OVERSEER['port'])
         def HandleFunc(self,conn,ip):
                 print("MyTcpServer:you got a handler"+str(ip))
-                conn.send(">>Welcome To Process Overseer:\n1.stop all process\n2.show proccesslist\n3.exit\n>>")
+                conn.send(">>Welcome To Process Overseer:\n1.stop all process safely\n2.show proccesslist\n3.exit\n>>")
                 while True:
                         try:
                                 r=int(conn.recv(1024))
@@ -38,9 +40,9 @@ class New(TcpServer.TcpServer):
                         elif r == 2:
                                 info="Total:"+str(len(self.WORKERS))+"\nNAME\t\tPID\t\tALIVE\n"
                                 print self.WORKERS
-                                for i in self.WORKERS:
-                                        print i.name
-                                        info=info+i.name+"\t\t"+str(i.pid)+"\t\t"+str(i.is_alive())+"\n"
+                                for i in self.status:
+                                        print i[0]
+                                        info=info+i[0]+"\t\t"+str(i[1])+"\t\t"+str(i[2])+"\n"
                                 info+=">>"
                                 conn.send(info)
                         elif r == 3:
@@ -56,25 +58,25 @@ class New(TcpServer.TcpServer):
 	def __Worker(self,jq,rq,):
 		if self.WORKERMODE == "persist":
 			while not self.KILL.get():
-				print self.KILL.get()
 				rq.put(self.WORKER(jq.get(block=True)))
 		elif self.WORKERMODE == "finish":
 			while  not self.KILL.get() and not jq.empty():
-				print self.KILL.get()
 				try:
 					rq.put(self.WORKER(jq.get(block=False)))
 				except:
 					continue
-		return 0
+		sys.exit()
+		return 
 	def Start(self):
-		if self.OVERSEER['status'] == True:
-			wd = multiprocessing.Process(target=self.__Overseer)
-			wd.start()
 		for proc in xrange(0,self.PROCESS):
 			prc = multiprocessing.Process(target=self.__Worker,args=(self.JOBQUE,self.RESULTQUE))                           
 			prc.start()
 			self.WORKERS.append(prc)		
-		self.__Overseer()
+		if self.OVERSEER['status'] == True:
+			for  i in self.WORKERS:
+				self.status.append((i.name,i.pid,i.is_alive()))
+			wd = multiprocessing.Process(target=self.__Overseer)
+			wd.start()
 		try:                    
 			for worker in self.WORKERS:
 				worker.join()
